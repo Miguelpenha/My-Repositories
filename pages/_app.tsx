d350afdb90a16708f2acb987b869e22d576a5612
Miguel Penha <miguel.penha1@gmail.com>
2022-07-01 01:24:34 -0300

    Teste

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ const MyRepositories: FC<AppProps> = ({ Component, pageProps }) => {
           <meta charSet="UTF-8"/>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <link rel="icon" href="/favicon.ico"/>
         </Head>
         <Component {...pageProps}/>
         <GlobalStyle/>
