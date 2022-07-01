c2c5d0c738b32276391c237a8fa9c26718b96fc4
Miguel Penha <miguel.penha1@gmail.com>
2022-07-01 01:18:58 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ const MyRepositories: FC<AppProps> = ({ Component, pageProps }) => {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           {/* Favicons */}
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+          <link rel="shortcut icon" href="/favicon.ico"/>
         </Head>
         <Component {...pageProps}/>
         <GlobalStyle/>
