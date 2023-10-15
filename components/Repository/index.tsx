9198747cd1f8c5bcc61067388cbb17439cc47e92
Miguel Penha <miguel.penha1@gmail.com>
2023-10-15 01:21:41 -0300

    Ajeitando algumas coisas

--- components/Repository/index.tsx
+++ components/Repository/index.tsx
@@ -1,7 +1,7 @@
 import { IRepository } from '../../types'
 import { FC } from 'react'
 import api from '../../api'
-import { Container, Header, Title, Description, Languages, ContainerLanguage, Language, LanguageDetail, LoadingLanguage, Thumbnail, Markdown } from './style'
+import { Container, Header, Title, Description, Languages, ContainerLanguage, Language, LanguageDetail, LoadingLanguage, Options, Option, IconOption, Thumbnail, Markdown } from './style'
 import useThumbnail from './useThumbnaill'
 import useMarkdown from './useMarkdown'
 
@@ -14,8 +14,6 @@ const Repository: FC<Iprops> = ({ repository, homePage }) => {
     const { data: languages } = api.get<object>(repository.languages_url)
     const thumbnail = useThumbnail(homePage)
     const markdown = useMarkdown(thumbnail, repository.name)
-
-    // href={repository.html_url} target="_blank" title={repository.name}
     
     return (
         <Container>
@@ -30,6 +28,22 @@ const Repository: FC<Iprops> = ({ repository, homePage }) => {
                         </ContainerLanguage>
                     )) : <LoadingLanguage size={20} borderSize={3}/>}
                 </Languages>
+                <Options>
+                    <Option href={repository.html_url} target="_blank" title={repository.name}>
+                        <IconOption xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/>
+                        </IconOption>
+                    </Option>
+                    {repository.homepage && (
+                        <Option href={homePage} target="_blank" title={homePage} rel="nofollow noreferrer">
+                            <IconOption xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path d="M0 0h24v24H0V0z" fill="none"/>
+                                <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
+                            </IconOption>
+                        </Option>
+                    )}
+                </Options>
                 {thumbnail && <Thumbnail src={thumbnail} width={1200} height={630}/>}
                 {!thumbnail && markdown && (
                     <Markdown children={markdown}/>
--- components/Repository/style.ts
+++ components/Repository/style.ts
@@ -6,7 +6,7 @@ import MarkdownRaw from 'react-markdown'
 export const Container = styled.div`
     width: 85%;
     margin: auto;
-    padding: 1.5em;
+    padding: 1em;
     align-self: center;
     height: min-content;
     border-radius: 20px;
@@ -56,29 +56,57 @@ export const ContainerLanguage = styled.div`
     display: flex;
     margin-right: 4%;
     margin-top: 1.5%;
+    position: relative;
     align-items: center;
     scroll-snap-align: center;
 `
 
 export const LanguageDetail = styled.div`
-    width: 1.1em;
+    width: 0.8em;
     height: 0.8em;
     margin-right: 6%;
     min-width: 0.8vw;
     border-radius: 50%;
+    position: absolute;
     background-color: ${props => props.theme.primary};
 `
 
 export const Language = styled.div`
     font-weight: bold;
     font-size: 1.1rem;
+    margin-left: 1.1em;
 `
 
 export const LoadingLanguage = styled(Loading)`
     left: 0%;
     transform: none;
-    position: sticky;
     margin-top: 1em;
+    position: sticky;
+`
+
+export const Options = styled.div`
+    gap: 1em;
+    display: flex;
+    margin-bottom: 1.5em;
+`
+
+export const Option = styled.a`
+    width: 3em;
+    display: flex;
+    padding: 0.5em;
+    border-radius: 25%;
+    transition-duration: 0.2s;
+    transition-timing-function: linear;
+    
+    :hover {
+        border-radius: 50%;
+        background-color: ${props => props.theme.glass};
+    }
+`
+
+export const IconOption = styled.svg`
+    width: 100%;
+    fill: ${props => props.theme.primary};
 `
 
 export const Thumbnail = styled(Image)`
